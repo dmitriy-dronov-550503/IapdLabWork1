068040444603e768779c11857d37c22fb0cdce04
Dmitriy Dronov <bsuir.dmitriy.dronov@gmail.com>
2017-10-07 10:55:42 +0300

    Выполнен ввод "сырых" данных об устройствах на шине, с помощью WMI.

--- IapdLabWork1/Program.cs
+++ IapdLabWork1/Program.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Management;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace IapdLabWork1
 {
@@ -10,6 +14,16 @@ namespace IapdLabWork1
     {
         static void Main(string[] args)
         {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_PnPEntity");
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                foreach (PropertyData property in queryObj.Properties)
+                {
+                    Console.WriteLine("{0}: {1}", property.Name, property.Value);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
