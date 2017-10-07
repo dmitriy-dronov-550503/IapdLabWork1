41dc31646e63cbbe893a151579de3de54f451248
Dmitriy Dronov <bsuir.dmitriy.dronov@gmail.com>
2017-10-07 10:57:32 +0300

    Добавлен класс DeviceInformation, содержащий информацию о PCI устройстве.

---
+++ IapdLabWork1/DeviceInformation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IapdLabWork1
+{
+    class DeviceInformation
+    {
+        private string name { get; set; }
+        private string description;
+        private Dictionary<string, string> parameters;
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public void setDescription(string description)
+        {
+            this.description = description;
+        }
+
+        public void setParameters(Dictionary<string, string> parameters)
+        {
+            this.parameters = parameters;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getDesription()
+        {
+            return description;
+        }
+
+        public Dictionary<string, string> getParameters()
+        {
+            return parameters;
+        }
+    }
+}
